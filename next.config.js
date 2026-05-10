a281d7875e85bf88523e24b47ecb0d53202845b9
lucasrafagnin <lucasrafagnin7@gmail.com>
2026-05-10 19:34:25 -0300

    chore: setu

--- next.config.js
+++ next.config.js
@@ -17,19 +17,6 @@ export default {
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;"
   },
 
-  // webpack: (config) => {
-  //   // Workaround for ensuring that `react` and `react-dom` resolve correctly
-  //   // when using a locally-linked version of `react-notion-x`.
-  //   // @see https://github.com/vercel/next.js/issues/50391
-  //   const dirname = path.dirname(fileURLToPath(import.meta.url))
-  //   config.resolve.alias.react = path.resolve(dirname, 'node_modules/react')
-  //   config.resolve.alias['react-dom'] = path.resolve(
-  //     dirname,
-  //     'node_modules/react-dom'
-  //   )
-  //   return config
-  // },
-
-  // See https://react-tweet.vercel.app/next#troubleshooting
-  transpilePackages: ['react-tweet']
+  transpilePackages: ['react-tweet'],
+  basePath: '/blog'
 }
--- site.config.ts
+++ site.config.ts
@@ -2,19 +2,19 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '35ca92cfcef78028b0effcb95160e8c4',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'RepFit Blog',
+  domain: 'repfit.io',
+  author: 'Lucas Rafagnin',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: 'A maior plataforma para personal trainers no Brasil',
 
   // social usernames (optional)
   twitter: 'transitive_bs',
