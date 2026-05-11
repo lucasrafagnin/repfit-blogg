0bf865938fee81515f3c8d258cd264becc3a8dd8
lucasrafagnin <lucasrafagnin7@gmail.com>
2026-05-10 23:04:05 -0300

    improve footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,22 +6,12 @@ import { LinkedInIcon } from '@/lib/icons/linkedin'
 import { MoonIcon } from '@/lib/icons/moon'
 import { SunIcon } from '@/lib/icons/sun'
 import { TwitterIcon } from '@/lib/icons/twitter'
-import { useDarkMode } from '@/lib/use-dark-mode'
 
 import styles from './styles.module.css'
 
 export function FooterImpl() {
   const [hasMounted, setHasMounted] = React.useState(false)
-  const { isDarkMode, toggleDarkMode } = useDarkMode()
   const currentYear = new Date().getFullYear()
-
-  const onToggleDarkMode = React.useCallback(
-    (e: any) => {
-      e.preventDefault()
-      toggleDarkMode()
-    },
-    [toggleDarkMode]
-  )
 
   React.useEffect(() => {
     setHasMounted(true)
@@ -31,20 +21,6 @@ export function FooterImpl() {
     <footer className={styles.footer}>
       <div className={styles.copyright}>
         Copyright {currentYear} {config.author}
-      </div>
-
-      <div className={styles.settings}>
-        {hasMounted && (
-          <a
-            className={styles.toggleDarkMode}
-            href='#'
-            role='button'
-            onClick={onToggleDarkMode}
-            title='Toggle dark mode'
-          >
-            {isDarkMode ? <MoonIcon /> : <SunIcon />}
-          </a>
-        )}
       </div>
 
       <div className={styles.social}>
